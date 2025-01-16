bf198ee5b7a31dbd35d7b74626750d8514dccca1
Arturo-MT <ingatorrez@gmail.com>
2025-01-15 18:17:29 -0600

    feat: registrar el modelo 'Store' en el panel de administración de pedidos

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from orders.models import Order, OrderItem, Product, ProductCategory
+from orders.models import Order, OrderItem, Product, ProductCategory, Store
 
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Product)
 admin.site.register(ProductCategory)
+admin.site.register(Store)
