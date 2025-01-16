65030f6662d4fa7799fbfd5606361168e559deaa
Arturo-MT <ingatorrez@gmail.com>
2025-01-15 18:16:59 -0600

    feat: agregar nuevas vistas y rutas para el panel de control, impresión de tickets y gestión de tiendas

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from orders.views import ProductCategoryViewSet, ProductViewSet, OrderItemViewSet, OrderViewSet, CreateOrderView, HomePageView
+from orders.views import ProductCategoryViewSet, ProductViewSet, OrderItemViewSet, OrderViewSet, CreateOrderView, HomePageView, DashboardView, PrintTicketView, StoreViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -12,13 +12,16 @@ router.register(r'product-categories', ProductCategoryViewSet)
 router.register(r'products', ProductViewSet)
 router.register(r'order-items', OrderItemViewSet)
 router.register(r'orders', OrderViewSet)
+router.register(r'stores', StoreViewSet)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/auth/', include('authentication.urls')),
-    path('api/create-order/', CreateOrderView.as_view(), name='create-order'),
+    path('api/create-order/', CreateOrderView.as_view(), name='create_order'),
+    path('api/print-ticket/', PrintTicketView.as_view(), name='print_ticket'),
 ]
 
 if settings.DEBUG:
