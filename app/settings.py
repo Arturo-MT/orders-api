d8fc3cc29d12668f871d4914d24b70001eb33643
Arturo Monroy <ingatorrez@gmail.com>
2025-01-31 15:37:06 -0600

    Config nginx to manage ssl

--- app/settings.py
+++ app/settings.py
@@ -20,6 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # Media files
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
