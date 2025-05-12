c7b2a88b5bcc10da40795e9882a91d1322b39615
Arturo Monroy <ingatorrez@gmail.com>
2025-05-12 12:51:43 -0600

    refactor: add token verification endpoint and organize urlpatterns

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,21 +1,26 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from .views import CurrentUserView
 
 from .views import LoginView, LogoutView, RegisterView, SettingsTemplateView
 
 urlpatterns = [
-     path('login/',
-          LoginView.as_view(), name='auth_login'),
-     path('logout/',
-          LogoutView.as_view(), name='auth_logout'),
-     path('register/',
-          RegisterView.as_view(), name='auth_register'),
-     path('reset/', 
-          include('django_rest_passwordreset.urls', namespace='password_reset')),
-     path('settings/',
-          SettingsTemplateView.as_view(), name='account_settings'),
-     path('token',
-          TokenObtainPairView.as_view(), name='token_obtain_pair'),
-     path('token/refresh/',
-          TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/',
+         LoginView.as_view(), name='auth_login'),
+    path('logout/',
+         LogoutView.as_view(), name='auth_logout'),
+    path('register/',
+         RegisterView.as_view(), name='auth_register'),
+    path('reset/',
+         include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('settings/',
+         SettingsTemplateView.as_view(), name='account_settings'),
+    path('token/',
+         TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/',
+         TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/',
+         TokenVerifyView.as_view(), name='token_verify'),
+    path('me/',
+         CurrentUserView.as_view(), name='user_me'),
 ]
