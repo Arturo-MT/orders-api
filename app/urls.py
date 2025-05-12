63ef1f51364b9d3a3ad243f1d59835db05298d28
Arturo Monroy <ingatorrez@gmail.com>
2025-05-12 12:50:00 -0600

    refactor: clean up urls.py by removing unused views and organizing imports

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from orders.views import ProductCategoryViewSet, ProductViewSet, OrderItemViewSet, OrderViewSet, StoreViewSet
-from orders.views import CategoryTemplateView, HomePageView, DashboardView, PrintTicketView, ProductTemplateView, CreateOrderTemplateView
+from orders.views import ProductCategoryViewSet, ProductViewSet, OrderItemViewSet, OrderViewSet, StoreViewSet, CreateOrderView, DashboardAPIView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -16,17 +15,18 @@ router.register(r'orders', OrderViewSet)
 router.register(r'stores', StoreViewSet)
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    #    path('', HomePageView.as_view(), name='home'),
+    #     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/auth/', include('authentication.urls')),
-    path('api/create-order/', CreateOrderTemplateView.as_view(), name='create_order'),
-    path('api/print-ticket/', PrintTicketView.as_view(), name='print_ticket'),
-    path('categories/', CategoryTemplateView.as_view(),
-         name='category_template_view'),
-    path('products/', ProductTemplateView.as_view(),
-         name='product_template_view'),
+    path('api/create-order/', CreateOrderView.as_view(), name='create_order'),
+    path('api/dashboard/', DashboardAPIView.as_view(), name='dashboard_api'),
+    #    path('api/print-ticket/', PrintTicketView.as_view(), name='print_ticket'),
+    #    path('categories/', CategoryTemplateView.as_view(),
+    #         name='category_template_view'),
+    #    path('products/', ProductTemplateView.as_view(),
+    #         name='product_template_view'),
 ]
 
 if settings.DEBUG:
